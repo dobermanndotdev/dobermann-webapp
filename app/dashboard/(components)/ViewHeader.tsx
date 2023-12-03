2067e1e0310396791d50cdc05e8ae038a8f2ed09
Firmino Changani <firmino.changani@gmail.com>
2023-12-03 22:11:31 +0000

    feat(#30): list monitors

--- app/dashboard/(components)/ViewHeader.tsx
+++ app/dashboard/(components)/ViewHeader.tsx
@@ -7,7 +7,7 @@ interface Props extends PropsWithChildren {
 export function ViewHeader({ children, title }: Props) {
   return (
     <section>
-      <h2 className="font-bold text-2xl mb-1">{title}</h2>
+      <h2 className="font-bold text-2xl mb-6">{title}</h2>
       <div className="text-sm">{children}</div>
     </section>
   );

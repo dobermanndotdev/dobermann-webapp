2079caf8dfd9cdf225bac829693e3f1ace738d98
Firmino Changani <firmino.changani@gmail.com>
2024-01-06 19:20:36 +0000

    feat(#71): add stat to show the uptime

--- src/pages/dashboard/monitors/[monitorId]/index.tsx
+++ src/pages/dashboard/monitors/[monitorId]/index.tsx
@@ -76,7 +76,7 @@ export default function MonitorPage({ monitor: initialData, responseTimeStats }:
       </PageTitle>
 
       <Grid columns="3" gap="4" width="auto" mb="5">
-        <Stat label="Uptime" value={monitor.up_since ? Dates.fromNow(monitor.up_since, true) : "-"} />
+        <Stat label="Up for" value={monitor.up_since ? Dates.fromNow(monitor.up_since, true) : "-"} />
         <Stat label="Last checked at" value={<LiveLastCheckedAt value={monitor.last_checked_at || ""} />} />
         <Stat label="Incidents" value={monitor.incidents.length || 0} />
       </Grid>
